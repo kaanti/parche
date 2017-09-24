51c4b72e1916d861b236e12f0a2be56ca6e78531
Dhruv Bhanushali <dhruv_b@live.com>
2017-09-25 00:28:07 +0530

    Initial commit

---
+++ src/main/java/dhruvkb/parche/ParcheApplication.java
@@ -0,0 +1,12 @@
+package dhruvkb.parche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcheApplication.class, args);
+	}
+}
